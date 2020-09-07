a9ac3694d37d845df514a424320f095dbeb4aab1
Piotr Orlowski <p.orlowski1@gmail.com>
2020-09-07 21:44:21 +0200

    add multiply function

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const firstInput = document.getElementById('FirstInput');
 const secondInput = document.getElementById('SecondInput');
 const result = document.getElementById('ResultElement');
 
+const button_multiply = document.getElementById('ButtonElement_multiply');
+const firstInput_multiply = document.getElementById('FirstInput_multiply');
+const secondInput_multiply = document.getElementById('SecondInput_multiply');
+const result_multiply = document.getElementById('ResultElement_multiply');
+
 // Define add function.
 
 const addFunction = function () {
@@ -23,10 +28,24 @@ const addFunction = function () {
 };
 
 // Define multiply function.
+const multiplyFunction = function () {
+  // Get values from inputs
+  const firstValue = firstInput_multiply.value;
+  const secondValue = secondInput_multiply.value;
+  // Check if those values.lengths are different than 0.
+  if (firstValue.length === 0 && secondValue.length === 0) {
+    result_multiply.innerHTML = 'No values were provided in inputs';
+  } else {
+    // If those values.length are different than 0 then do sum operation with '+' operator.
+    sum = parseInt(firstValue) * parseInt(secondValue);
+    // Set innerHTML of paragraph to firstValue + secondValue = sum.
+    result_multiply.innerHTML = `${firstValue} * ${secondValue} = ${sum}`;
+  }
+};
 
 // Define divide function.
 
-// Define module function.
+// Define modulo function.
 
 // Define exponentiation function.
 
@@ -34,3 +53,4 @@ const addFunction = function () {
 
 // Add event listener for a button element with addFunction event handler.
 button.addEventListener('click', addFunction, false);
+button_multiply.addEventListener('click', multiplyFunction, false);
